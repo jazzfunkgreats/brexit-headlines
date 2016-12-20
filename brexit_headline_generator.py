4f838bf0c7f39f67b0fcd2fc078b7f6a0e466346
jazzfunkgreats <tom.bennett182@googlemail.com>
2016-12-20 15:43:21 +0000

    Add files via upload

---
+++ brexit_headline_generator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+###################################################
+#
+#	Brexit Headliner Generator
+#
+#	Description:
+#		Makes stupid headline puns based on films
+#		that have 'You' in the title.
+#
+###################################################
+
+import requests, random, bs4, re
+
+## 1. Grab the IMDB search results for "You"
+imdb_link = 'http://www.imdb.com/find?q=you&s=tt&ref_=fn_al_tt_mr'
+r = requests.get(imdb_link)
+r.raise_for_status()
+site_data = r.text
+titles = []
+
+## 2. Parse the search results and strain out the juicy titles
+soup = bs4.BeautifulSoup(site_data, 'lxml')
+elems = soup.select('.result_text a')
+for elem in elems:
+	title = elem.get_text()
+	titles.append(title)
+
+## 3. Pick a random title and replace 'You' with 'EU'
+while True:
+	num = random.randint(0, len(titles)-1)
+	chosen_title = titles[num]
+	if not 'You' in chosen_title:
+		continue
+	if (chosen_title == 'You'):
+		continue
+	print(chosen_title.replace('You', 'EU'))
+	break
